b13dddfae068e4ae31d8f7104e81998e2c057b6f
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-08-26 23:24:36 -0300

    Create Escola.cs

---
+++ Escola.cs
@@ -0,0 +1,17 @@
+namespace ProjEscola{
+    class Escola{
+        private Curso[5] cursos;
+
+         public bool adicionarCurso{
+
+        }
+
+        public bool removerCurso{
+            
+        }
+
+        public Curso pesquisarCurso{
+            
+        }
+    }
+}
