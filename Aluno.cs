cfe0cd3b2d02cc2353bff3bf9eb88af4c50c148d
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-08-26 22:27:30 -0300

    Create Aluno.cs

---
+++ Aluno.cs
@@ -0,0 +1,18 @@
+class Aluno {
+        private int id;
+        private string nome;
+
+        public string Id{
+            get {return id;}
+            set {name = id;}
+        }
+
+        public string Nome{
+            get {return name;}
+            set {name = value;}
+        }
+
+        public bool podeMatricular(Cursos cursos){
+
+        }
+    }
